f0bede588e14982e9b526c72ba83f41166f3b7e1
Diego Semensato O Cardoso <dsocardoso@gmail.com>
2021-04-27 07:20:01 -0300

    including model files

---
+++ app/src/main/java/br/com/dsocardoso/foodrecipes/model/FoodRecipe.kt
@@ -0,0 +1,8 @@
+package br.com.dsocardoso.foodrecipes.model
+
+import com.google.gson.annotations.SerializedName
+
+data class FoodRecipe(
+    @SerializedName("results")
+    val results: List<Result>
+)
