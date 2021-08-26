c9c55cd3ae7e4ec442161edad207bf5f0a502b98
Diego Semensato O Cardoso <dsocardoso@gmail.com>
2021-08-26 09:32:09 -0300

    Instructions Fragment - Calling Webview + some layout adjusts

--- app/src/main/java/br/com/dsocardoso/foodrecipes/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/br/com/dsocardoso/foodrecipes/ui/fragments/instructions/InstructionsFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import br.com.dsocardoso.foodrecipes.R
+import br.com.dsocardoso.foodrecipes.model.Result
+import br.com.dsocardoso.foodrecipes.util.Constants
+import kotlinx.android.synthetic.main.fragment_instructions.view.*
 
 class InstructionsFragment : Fragment() {
 
@@ -14,7 +18,16 @@ class InstructionsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        val view = inflater.inflate(R.layout.fragment_instructions, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(Constants.RECIPE_RESULT_KEY)
+
+        view.instructions_webView.webViewClient = object : WebViewClient() {}
+        val webSiteUrl: String = myBundle!!.sourceUrl
+        view.instructions_webView.loadUrl(webSiteUrl)
+
+        return view
     }
 
 }
